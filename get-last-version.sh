0fdd8d7903aa79a2186e9eb0362a78645684a1c5
Andrey Chalkin <L2jLiga@gmail.com>
2024-04-14 19:47:21 +0200

    chore: fix some linter issues

--- get-last-version.sh
+++ get-last-version.sh
@@ -1,17 +1,17 @@
-latest_version=$(git tag | tail -1)
+latest_version="$(git tag | tail -1)"
 echo 0
-is_major=$(git log $latest_version..HEAD --oneline | cut -d ' ' -f2 | grep '!:' -c)
-is_minor=$(git log $latest_version..HEAD --oneline | cut -d ' ' -f2 | grep 'feat:' -c)
+is_major="$(git log $latest_version..HEAD --oneline | cut -d ' ' -f2 | grep '!:' -c)"
+is_minor="$(git log $latest_version..HEAD --oneline | cut -d ' ' -f2 | grep 'feat:' -c)"
 echo 1
 
 increment='patch'
 echo 2
 
-if ! [ $is_major = '0' ]; then
+if ! [ "$is_major" = '0' ]; then
   echo 2.1
   increment='major'
 else
-  if ! [ $is_minor = '0' ]; then
+  if ! [ "$is_minor" = '0' ]; then
     echo 2.2
     increment='minor'
   fi
@@ -19,12 +19,12 @@ fi
 echo 3
 
 npm i
-version=$(npm run semver $latest_version -i $increment | tail -1)
+version="$(npm run semver $latest_version -i $increment | tail -1)"
 echo 4
 
 echo "semver=$version" >>$GITHUB_OUTPUT
 echo 5
 echo "changes=<<EOF" >>$GITHUB_OUTPUT
-echo "$(git log $latest_version..HEAD --oneline)" >>$GITHUB_OUTPUT
+git log $latest_version..HEAD --oneline >>$GITHUB_OUTPUT
 echo "EOF" >>$GITHUB_OUTPUT
 echo 6
